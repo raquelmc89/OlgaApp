fa4d82a4421721dc3c7b2bfbf42be5421603bf77
Raquel Martin Calderon <raquelmartincalderon@Raquels-MacBook-Air.local>
2023-03-19 07:16:44 +0000

    singledaypicker done

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,9 +1,10 @@
-import { render } from "@testing-library/react";
-import React, { useState } from "react";
+import React from "react";
+import moment from "moment"
+import 'react-dates/initialize';
 import {SingleDatePicker} from 'react-dates';
 import PhoneInput from 'react-phone-number-input'
-import 'react-phone-number-input/style.css'
-import 'react-date/lib/css/_datepicker.css'
+import 'react-phone-number-input/style.css';
+import 'react-dates/lib/css/_datepicker.css';
 
 export default class ContactMe extends React.Component{
     constructor(props){
@@ -14,7 +15,9 @@ export default class ContactMe extends React.Component{
             comment:"",
             phone: "",
             mail:"",
-            calendarfocused:false,
+            date:"",
+            calendarFocused:false,
+            format: "dd/mm/yyy"
         }
     };
     onSubmit = (e)=>{
@@ -32,13 +35,22 @@ export default class ContactMe extends React.Component{
         if (!email || email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)){
             this.setState(()=>({email}))
         }
-    }
+    };
     handleComment = (e)=>{
         const comment = e.target.value;
-        if(!comment || comment.match(/^(?=.{0,30}$)([-'\w]+\s)*[-'\w]+$/)){
+        if(!comment || comment.match(/^(?=.{0,100}$)([-'\w]+\s)*[-'\w]+$/)){
             this.setState(()=>({comment}))
         }
     };
+    onDateChange = (date) => {
+        if (date) {
+            this.setState(()=>({date}))
+        }
+    };
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }));
+      };
+
     render(){
            return(
             <div>
@@ -46,39 +58,42 @@ export default class ContactMe extends React.Component{
             <form onSubmit={this.onSubmit}>
             <label> Name</label>
             <input 
-            type="text"
-            placeholder="Enter your full name"
-            autoFocus
-            value={this.state.name}
-            onChange={this.handleName}>
+             type="text"
+             placeholder="Enter your full name"
+             autoFocus
+             value={this.state.name}
+             onChange={this.handleName}>
             </input>
             <label>Phone number</label>
             <PhoneInput
-            type="tel"
-            placeholder="+44"
-            value={this.state.phone}
-            onChange={phone => this.setState({phone})}>
+             type="tel"
+             placeholder="+44"
+             value={this.state.phone}
+             onChange={phone => this.setState({phone})}>
             </PhoneInput>
             <label>Email adress</label>
             <input
-            type="email"
-            placeholder=""
-            value={this.state.mail}
-            onChange={this.handleMail}></input>
+             type="email"
+             placeholder=""
+             value={this.state.mail}
+             onChange={this.handleMail}></input>
             <label>Commemts</label>
             <textarea
-            type="text"
-            placeholder="max 100 words"
-            value={this.state.comment}
-            onChange={this.handleComment}>
+             type="text"
+             placeholder="max 100 words"
+             value={this.state.comment}
+             onChange={this.handleComment}>
             </textarea>
-           <SingleDatePicker
-           date=""
-           onDateChange=""
-           focused=""
-           onFocusChange=""
-           numberOfMonths={1}
-           isOutsideRange={()=>false}></SingleDatePicker>
+            <SingleDatePicker
+             date={this.state.date}
+             onDateChange={this.onDateChange}
+             focused={this.state.calendarFocused}
+             onFocusChange={this.onFocusChange}
+             numberOfMonths={1}
+             displayFormat= {this.state.format}
+             isOutsideRange={()=>false}>
+            </SingleDatePicker>
+       
             <button onClick={this.onSubmit}>Submit</button>
             </form>
             </div>
---
+++ src/playground/playground.js
@@ -0,0 +1,8 @@
+<SingleDatePicker
+   date={this.state.date}
+   onDateChange={this.onDateChage}
+   focused={this.state.focused}
+   onFocusChange={this.state.onFocusChange}
+   numberOfMonths={1}
+   isOutsideRange={()=>false}>
+    </SingleDatePicker>
